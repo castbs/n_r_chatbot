0c754ac5cada1cc43269bc472826cafa689692e0
castbs <39009236+castbs@users.noreply.github.com>
2019-01-31 19:09:25 -0500

    Add files via upload

--- chatbot1.py
+++ chatbot1.py
@@ -1,10 +1,8 @@
 import random
 import csv
-
 import re
 import time
 import sys
-
 
 class myClass:
     q_or_a = ""
@@ -16,8 +14,6 @@ class myClass:
         self.who = var2
         self.category = var3
 
-
-
 class pairings:
     user_response = ""
     agent_response = ""
@@ -26,7 +22,6 @@ class pairings:
         self.user_response = var1
         self.agent_response = var2
 
-
 with open('N_smalltalk_dataset.csv') as dataset_file:
     reader1 = csv.reader(dataset_file)
     next(reader1)
@@ -34,17 +29,14 @@ with open('N_smalltalk_dataset.csv') as dataset_file:
     for r1 in reader1:
         col1 = r1
         data.append(myClass(col1[0],col1[1],col1[2]))
-
-
 with open('rules.csv') as rules_file:
     reader2 = csv.reader(rules_file)
     next(reader2)
     pairings_array = []
     for r2 in reader2:
-        col2 = r2
-        #print(str(col2))
-        pairings_array.append(pairings(col2[0],col2[1]))
-
+       col2 = r2
+       #print(str(col2))
+       pairings_array.append(pairings(col2[0],col2[1]))
 
 def print_slowly(text):
     for letter in str(text):
@@ -54,12 +46,9 @@ def print_slowly(text):
         time.sleep(random_time)
     print()
 
-
-
-
 while True:
     userInput = input(">>>>> ")
-    userInput = str(userInput).replace("    "," ").replace("   "," ").replace("  "," ").strip()
+    userInput = str(userInput).replace("    "," ").replace("   "," ").replace("  "," ").strip().lower()
 
     for y in range(0,1):
         if userInput == "buh-bai":
@@ -70,13 +59,13 @@ while True:
         for data_row in data:
             if str(data_row.q_or_a) == str(userInput) and str(data_row.who) != "agent":
                 userInput_category = str(data_row.category)
-                #print("Your Category is ===> " + str(userInput_category))
+                print("Your Category is ===> " + str(userInput_category))
                 break
 
         for p in pairings_array:
             if str(p.user_response) == str(userInput_category):
                 agentOutput_category = str(p.agent_response)
-                #print("My Category is ------> " + str(agentOutput_category))
+                print("My Category is ------> " + str(agentOutput_category))
                 break
 
         what_to_respond = []
@@ -89,3 +78,4 @@ while True:
         agent_says = random.choice(what_to_respond)
         #print(str(agent_says))
         print_slowly(str(agent_says))
+        print( str(len(what_to_respond)))
